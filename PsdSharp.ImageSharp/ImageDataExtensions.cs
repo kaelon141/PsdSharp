24d50ab7556bf7d5bab8184bcd4b2a589e5126ca
kaelon141 <28660369+kaelon141@users.noreply.github.com>
2025-10-08 04:55:38 +0200

    add ImageSharp plugin
    closes #8

---
+++ PsdSharp.ImageSharp/ImageDataExtensions.cs
@@ -0,0 +1,15 @@
+﻿using PsdSharp.Images;
+using PsdSharp.Images.DataConversion;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace PsdSharp.ImageSharp;
+
+public static class ImageDataExtensions
+{
+   public static Image<Rgba32> ToImageSharpImage(this ImageData imageData)
+   {
+      var interleavedBuffer = PixelDataConverter.GetInterleavedBuffer(imageData, ColorType.Rgba8888);
+      return Image.LoadPixelData<Rgba32>(interleavedBuffer, (int)imageData.Width, (int)imageData.Height);
+   }
+}
